d0a4864e703f727e879bd1e3f0d8401895ee1f9f
saurabhcedcoss <saurabhtripathi@cedcoss.com>
2022-10-05 18:09:38 +0530

    clear and input function updated

--- src/calscript.js
+++ src/calscript.js
@@ -1,1 +1,19 @@
 // javascript file for clculator
+var inputcontent = "";
+function inputval(input) {
+  if (input.value == "*" || "/" || "-" || "+") {
+    inputcontent += input.value;
+  } else {
+    inputcontent += parseInt(input.value);
+  }
+  document.getElementById("input").value = inputcontent;
+}
+//clearfunction
+function clearfn() {
+  document.getElementById("input").value = "";
+  inputcontent = "";
+}
+//equalfunction
+function equalfn(){
+    //fgdfgdf
+}
