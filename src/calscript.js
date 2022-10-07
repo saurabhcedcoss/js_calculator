a77184471391ccb24d72908196a01814d73b1428
saurabhcedcoss <saurabhtripathi@cedcoss.com>
2022-10-07 11:02:01 +0530

    Calculator complete code with ADD SUBSTRACT MULTIPLY DIVIDE AND CLEAR FUNCTION

--- src/calscript.js
+++ src/calscript.js
@@ -1,19 +1,44 @@
 // javascript file for clculator
-var inputcontent = "";
-function inputval(input) {
-  if (input.value == "*" || "/" || "-" || "+") {
-    inputcontent += input.value;
-  } else {
-    inputcontent += parseInt(input.value);
-  }
-  document.getElementById("input").value = inputcontent;
-}
-//clearfunction
+var inNumbf = "";
+var hisArr = [];
 function clearfn() {
-  document.getElementById("input").value = "";
-  inputcontent = "";
+  document.getElementById("dispe").innerHTML = "";
+  document.getElementById("inputNum").innerHTML = "";
+  document.getElementById("result").innerHTML = "";
+  inNumbf = "";
+  hisArr = [];
 }
-//equalfunction
-function equalfn(){
-    //fgdfgdf
+
+function inputval(type) {
+  inNumbf += type.value;
+  document.getElementById("inputNum").innerHTML = inNumbf;
+  if (type.id == "plus") {
+    hisArr.push(inNumbf);
+    inNumbf = "";
+    document.getElementById("inputNum").innerHTML = "";
+  }
+  if (type.id == "minus") {
+    hisArr.push(inNumbf);
+    inNumbf = "";
+  }
+  if (type.id == "multiply") {
+    hisArr.push(inNumbf);
+    inNumbf = "";
+  }
+  if (type.id == "divide") {
+    hisArr.push(inNumbf);
+    inNumbf = "";
+  }
+  if (type.id == "equal") {
+    hisArr.push(inNumbf);
+    inNumbf = "";
+    document.getElementById("inputNum").innerHTML = "";
+    var expRession = hisArr.toString().replace(/,/g, "");
+    var result = Function("return " + expRession)();
+    document.getElementById("result").innerHTML = result;
+  }
+
+  document.getElementById("dispe").innerHTML = hisArr
+    .toString()
+    .replace(/,/g, "");
 }
